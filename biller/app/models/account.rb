ec4647a15fc0314e89f73e64036810aac8a25cd4
kevinegstorf <kevinegstorf@kevins-MacBook-Pro.local>
2014-09-09 20:13:08 +0200

    added callback

--- biller/app/models/account.rb
+++ biller/app/models/account.rb
@@ -2,8 +2,8 @@ class Account < ActiveRecord::Base
   has_many :account_entries
 
   validates :name, 	presence: true, 
-  					length: {in:1..70
-  								message: "Please use a decent name"}, 
+  					length: {in:1..70},
+  					# 			message: "Please use a decent name", 
   					uniqueness: true
   validate :your_name_is_not_dumb
 
